b63c0af9d9c8289d3472893b388fe26f98ee085d
PhoonThetPine <thetpine254@gmail.com>
2023-11-03 16:43:30 +0800

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Phoon Thet Pine on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
